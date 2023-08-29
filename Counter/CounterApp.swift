1a6889be46090c9b5ee68543fa6727e0143980b9
Kawarai-S <kayawakakururu@icloud.com>
2023-08-29 21:54:47 +0900

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by 瓦井つばさ on 2023/08/29.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
