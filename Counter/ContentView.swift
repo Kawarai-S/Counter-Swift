bde1821adba8a10624f1e4722b1a2fbc598cea35
Kawarai-S <kayawakakururu@icloud.com>
2023-08-29 22:19:58 +0900

    View作成

--- Counter/ContentView.swift
+++ Counter/ContentView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.yellow
+                .ignoresSafeArea()
+            VStack {
+                Text("0")
+                    .font(.system(size: 130, weight: .bold, design: .monospaced))
+                HStack{
+                    Button{
+                        
+                    } label: {
+                        Text("-")
+                            .frame(height:  UIScreen.main.bounds.height/2.5)
+                            .frame(maxWidth: .infinity)
+                            .background(.blue)
+                            .foregroundColor(.white)
+                            .font(.system(size: 130, weight: .bold, design: .monospaced))
+                            .cornerRadius(10)
+                            
+                    }
+                    Button{
+                        
+                    } label: {
+                        Text("+")
+                            .frame(height:  UIScreen.main.bounds.height/2.5)
+                            .frame(maxWidth: .infinity)
+                            .background(.green)
+                            .foregroundColor(.white)
+                            .font(.system(size: 130, weight: .bold, design: .monospaced))
+                            .cornerRadius(10)
+                    }
+                }
+                Spacer()
+                Button{
+                    
+                } label: {
+                    Text("リセットする")
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 50)
+                        .background(.white)
+                        .foregroundColor(.red)
+                        .cornerRadius(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                            .stroke(.red, lineWidth: 1))
+                        
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
